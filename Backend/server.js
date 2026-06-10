0038ce83ccc504c1552c93d5ccc56ac588b33aac
abhishek20022 <abhishekkrsam324@gmail.com>
2026-06-10 17:14:36 +0530

    user schema make

--- Backend/server.js
+++ Backend/server.js
@@ -16,16 +16,15 @@ connectToDb() //database connection
 app.use(express.json()); 
 
 
-
-
-
-
-
-
 app.get("/test",(req,res)=>{
-      res.status(200).json({msg:"this is test Route"});
+    res.status(200).json({msg:"this is test Route"});
 })
 
 app.listen(PORT,()=>{
-    console.log(`server is started ${PORT}`)
+  console.log(`server is started ${PORT}`)
 })
+
+
+
+
+
